383cffcb771016e27ee5ade4ff1f74a4611e7718
HaiwangYu <yuhw.pku@gmail.com>
2020-02-19 21:48:43 -0500

    init

---
+++ pytorch/inc/WireCellPytorch/TorchScript.h
@@ -0,0 +1,40 @@
+/** A wrapper for pytorch torchscript
+ */
+
+#ifndef WIRECELLPYTORCH_TORCHSCRIPT
+#define WIRECELLPYTORCH_TORCHSCRIPT
+
+#include "WireCellIface/IConfigurable.h"
+#include "WireCellPytorch/ITorchScript.h"
+#include "WireCellUtil/Logging.h"
+
+#include <torch/script.h> // One-stop header.
+
+namespace WireCell {
+namespace Pytorch {
+class TorchScript : public ITorchScript, public IConfigurable {
+public:
+  TorchScript();
+  virtual ~TorchScript() {}
+
+  // IConfigurable interface
+  virtual void configure(const WireCell::Configuration &config);
+  virtual WireCell::Configuration default_configuration() const;
+
+  // ITorchScript interface
+  virtual int ident() const { return m_ident; }
+  virtual torch::IValue forward(const std::vector<torch::IValue> &inputs);
+
+private:
+  int m_ident;
+  Log::logptr_t l;
+  Configuration m_cfg; /// copy of configuration
+
+  torch::jit::script::Module m_module;
+
+  std::unordered_map<std::string, float> m_timers;
+};
+} // namespace Pytorch
+} // namespace WireCell
+
+#endif // WIRECELLPYTORCH_TORCHSCRIPT
