40d2908424c964e00778792903f25dc0a4c56686
HaiwangYu <yuhw.pku@gmail.com>
2020-03-24 01:13:49 -0400

    test util

---
+++ pytorch/test/test_util.cxx
@@ -0,0 +1,10 @@
+#include "WireCellPytorch/Util.h"
+
+using namespace WireCell;
+
+int main() {
+    auto ten = torch::ones({1, 3, 800, 600}, torch::dtype(torch::kFloat32).device(torch::kCUDA, 0));
+    std::cout << Pytorch::dump(ten) << "\n";
+
+    return 0;
+}
