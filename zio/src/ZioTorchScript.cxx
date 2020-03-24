296acb25bc177902babf876a41a52e06f6535f04
HaiwangYu <yuhw.pku@gmail.com>
2020-03-24 02:01:23 -0400

    cleanup

--- zio/src/ZioTorchScript.cxx
+++ zio/src/ZioTorchScript.cxx
@@ -46,49 +46,6 @@ void Pytorch::ZioTorchScript::configure(const WireCell::Configuration &cfg)
 
 namespace
 {
-// ITensorSet::pointer to_itensor(const std::vector<torch::IValue> &inputs)
-// {
-//     ITensor::vector *itv = new ITensor::vector;
-
-//     int ind = 0;
-//     for (auto ival : inputs)
-//     {
-//         auto ten = ival.toTensor().cpu();
-//         std::vector<size_t> shape = {(size_t)ten.size(1), (size_t)ten.size(2), (size_t)ten.size(3)};
-//         // TODO need to figure out type from dtyp
-//         Aux::SimpleTensor<float> *st = new Aux::SimpleTensor<float>(shape);
-//         size_t nbyte = 4;
-//         for (auto n : shape)
-//             nbyte *= n;
-//         auto data = (float *)st->data();
-//         memcpy(data, (float *)ten[0][0].data<float>(), nbyte);
-//         itv->push_back(ITensor::pointer(st));
-//         ++ind;
-//     }
-
-//     int seqno = 0;
-//     Configuration md;
-
-//     return std::make_shared<Aux::SimpleTensorSet>(seqno, md, ITensor::shared_vector(itv));
-// }
-// torch::IValue from_itensor(const ITensorSet::pointer &inputs)
-// {
-//     if (inputs->tensors()->size() != 1)
-//     {
-//         THROW(ValueError() << errmsg{"inputs->tensors()->size()!=1"});
-//     }
-//     auto iten = inputs->tensors()->front();
-//     if (iten->shape().size() != 4)
-//     {
-//         THROW(ValueError() << errmsg{"iten->shape().size()!=4"});
-//     }
-//     //TODO determine data type from metadata
-//     auto ten = torch::from_blob((float *)iten->data(), {(long)iten->shape()[0],
-//                                                         (long)iten->shape()[1],
-//                                                         (long)iten->shape()[2],
-//                                                         (long)iten->shape()[3]});
-//     return ten;
-// }
 std::string dump(const zio::Message &msg) {
     std::stringstream ss;
     ss << "zio.Message: ";
