065bfb4e43e14676d040c75931f67fe9abe063f4
Brett Viren <brett.viren@gmail.com>
2021-06-07 15:56:39 -0400

    Fix CLI11 usage

--- apps/apps/wcsonnet.cxx
+++ apps/apps/wcsonnet.cxx
@@ -15,62 +15,78 @@ int main(int argc, char* argv[])
 {
     CLI::App app{"wcsonnet is a Wire-Cell Toolkit aware Jsonnet compiler"};
 
-    std::vector<std::string> cfgfiles, load_path, extvars, extcode, tlavars, tlacode;
+    std::string filename;
+    std::vector<std::string> load_path, extvars, extcode, tlavars, tlacode;
 
     app.add_option("-P,--path", load_path,
-                   "Search paths to consider in addition to those in WIRECELL_PATH");
-
+                   "Search paths to consider in addition to those in WIRECELL_PATH")->type_size(1)->allow_extra_args(false);
     app.add_option("-V,--ext-str", extvars,
-                   "Jsonnet external variable as <name>=<string>");
-    app.add_option("-C,--ext-code", extvars,
-                   "Jsonnet external code as <name>=<string>");
+                   "Jsonnet external variable as <name>=<string>")->type_size(1)->allow_extra_args(false);
+    app.add_option("-C,--ext-code", extcode,
+                   "Jsonnet external code as <name>=<string>")->type_size(1)->allow_extra_args(false);
     app.add_option("-A,--tla-str", tlavars,
-                   "Jsonnet level argument value <name>=<string>");
-    app.add_option("--tla-code", tlacode,
-                   "Jsonnet level argument code <name>=<string>");
-    app.add_option("files", cfgfiles,
-                   "Jsonnet files to compile");
+                   "Jsonnet level argument value <name>=<string>")->type_size(1)->allow_extra_args(false);
+    app.add_option(",--tla-code", tlacode,
+                   "Jsonnet level argument code <name>=<string>")->type_size(1)->allow_extra_args(false);
+    app.add_option("file", filename, "Jsonnet file to compile");
                     
-    app.set_help_flag();
-    auto help = app.add_flag("-h,--help", "Print help message");
+    // app.set_help_flag();
+    // auto help = app.add_flag("-h,--help", "Print help message");
 
-    //CLI11_PARSE(app, argc, argv);
+    CLI11_PARSE(app, argc, argv);
 
-    try {
-        app.parse(argc, argv);
-        if (*help or cfgfiles.empty()) {
-            throw CLI::CallForHelp();
-        }
-    }
-    catch(const CLI::Error &e) {
-        return app.exit(e);
-    }
+    // try {
+    //     app.parse(argc, argv);
+    //     if (*help) {
+    //         throw CLI::CallForHelp();
+    //     }
+    // }
+    // catch(const CLI::Error &e) {
+    //     return app.exit(e);
+    // }
 
 
     std::map<std::string, std::string> m_extvars, m_extcode, m_tlavars, m_tlacode;
 
+    for (auto path : load_path) {
+        std::cerr << "path: " << path << std::endl;
+    }
+    std::cerr << "input: " << filename << std::endl;
+
     for (auto vev : extvars) {
+        std::cerr << "extvar: " << vev << std::endl;
         auto vv = String::split(vev, "=");
         m_extvars[vv[0]] = vv[1];
     }
     for (auto vev : extcode) {
+        std::cerr << "extcode: " << vev << std::endl;
         auto vv = String::split(vev, "=");
         m_extcode[vv[0]] = vv[1];
     }
     for (auto vev : tlavars) {
+        std::cerr << "tlavar: " << vev << std::endl;
         auto vv = String::split(vev, "=");
         m_tlavars[vv[0]] = vv[1];
     }
-    // Add any top-level argument code
     for (auto vev : tlacode) {
+        std::cerr << "tlacode: " << vev << std::endl;
         auto vv = String::split(vev, "=");
         m_tlacode[vv[0]] = vv[1];
     }
 
+    try {
+        if (filename.empty()) {
+            throw CLI::CallForHelp();
+        }
+    }
+    catch(const CLI::Error &e) {
+        std::cerr << "Must give at least one Jsonnet file to compile" << std::endl;
+        return app.exit(e);
+    }
     Persist::Parser parser(load_path, m_extvars, m_extcode,
                            m_tlavars, m_tlacode);
 
-    auto jdat = parser.load(argv[1]);
+    auto jdat = parser.load(filename);
     std::cout << jdat << std::endl;
 
     return 0;
