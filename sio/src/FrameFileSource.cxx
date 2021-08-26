8c6d7aee792c67a33a2cd70afd9181cd5d3a7971
Brett Viren <brett.viren@gmail.com>
2021-08-26 13:09:29 -0400

    Clean and test frame file source. Fix #111.

--- sio/src/FrameFileSource.cxx
+++ sio/src/FrameFileSource.cxx
@@ -11,8 +11,6 @@
 
 #include "WireCellAux/FrameTools.h"
 
-#include <tuple>
-
 WIRECELL_FACTORY(FrameFileSource, WireCell::Sio::FrameFileSource,
                  WireCell::INamed,
                  WireCell::IFrameSource, WireCell::IConfigurable)
@@ -75,15 +73,22 @@ bool Sio::FrameFileSource::matches(const std::string& tag)
 }
 
 // ident, type, tag, ext
-using filemd_t = std::tuple<int, std::string, std::string, std::string>;
+struct filemd_t {
+    std::string fname{""};
+    bool okay{false};
+    int ident{0};
+    std::string type{""}, tag{""}, ext{""};
+};
 
 static
 filemd_t parse_filename(const std::string& fname)
 {
+    if (fname.empty()) { return {fname}; }
     auto parts = split(fname, "_");
+    if (parts.size() != 3) { return {fname}; }
     auto rparts = split(parts[2], ".");
     int ident = std::atoi(rparts[0].c_str());
-    return std::make_tuple(ident, parts[0], parts[1], rparts[1]);
+    return filemd_t{fname, true, ident, parts[0], parts[1], rparts[1]};
 }
 
 static
@@ -102,6 +107,10 @@ IFrame::pointer Sio::FrameFileSource::next()
     // Suffer full eager loading
     pigenc::File fpig, cpig, tpig;
     auto fmd = read_pig(m_in, fpig);
+    if (m_in.eof()) {
+        log->debug("call={}, frame stream EOF with file={}", m_count, m_inname);
+        return nullptr;
+    }
     auto cmd = read_pig(m_in, cpig);
     auto tmd = read_pig(m_in, tpig);
 
@@ -109,27 +118,33 @@ IFrame::pointer Sio::FrameFileSource::next()
         log->error("call={}, frame read fail with file={}", m_count, m_inname);
         return nullptr;
     }
+    if (!fmd.okay) {
+        log->error("call={}, frame read fail to parse npy file name {} in file={}", m_count, fmd.fname, m_inname);
+        return nullptr;
+
+    }
 
     // fixme: here we assume f,c,t are properly lock-step ordered and
     // just check frame.
-    const std::string havetag = std::get<1>(fmd);
-    if (! matches(havetag)) {
+    if (! matches(fmd.tag)) {
         log->debug("call={}, skipping tag=\"{}\" file={}",
-                   m_count, havetag, m_inname);
+                   m_count, fmd.tag, m_inname);
         return next();
     }
-
-    const int ident = std::get<0>(fmd);
 
     std::vector<int> channels;
     bool ok = pigenc::stl::load(cpig, channels);
     if (!ok) {
+        log->error("call={}, channel load failed tag=\"{}\" file={}",
+                   m_count, fmd.tag, m_inname);
         return nullptr;
     }
 
     std::vector<double> tickinfo;
     ok = pigenc::stl::load(tpig, tickinfo);
     if (!ok) {
+        log->error("call={}, tickinfo load failed tag=\"{}\" file={}",
+                   m_count, fmd.tag, m_inname);
         return nullptr;
     }
     // time, tick, min tbin0 as doubles
@@ -143,6 +158,8 @@ IFrame::pointer Sio::FrameFileSource::next()
         Array::array_xxs sarr;
         ok = pigenc::eigen::load(fpig, sarr);
         if (!ok) {
+            log->error("call={}, short load failed tag=\"{}\" file={}",
+                       m_count, fmd.tag, m_inname);
             return nullptr;
         }
         arr = sarr.cast<float>();
@@ -150,12 +167,13 @@ IFrame::pointer Sio::FrameFileSource::next()
     else {
         ok = pigenc::eigen::load(fpig, arr);
         if (!ok) {
+            log->error("call={}, float load failed tag=\"{}\" file={}",
+                       m_count, fmd.tag, m_inname);
             return nullptr;
         }
     }        
 
     ITrace::vector all_traces;
-    IFrame::trace_list_t tagged_traces;
 
     for (long irow=0; irow < arr.rows(); ++irow) {
         int chid = channels[irow];
@@ -164,16 +182,17 @@ IFrame::pointer Sio::FrameFileSource::next()
         ITrace::ChargeSequence charges(row.data(), row.data() + arr.cols());
 
         auto itrace = std::make_shared<SimpleTrace>(chid, tbin0, charges);
-        if (! havetag.empty()) {
-            tagged_traces.push_back(all_traces.size());
-        }
         all_traces.push_back(itrace);
     }
 
-    auto sframe = new SimpleFrame(ident, time, all_traces, tick);
-    if (! havetag.empty()) {
-        sframe->tag_traces(havetag, tagged_traces);
+    auto sframe = new SimpleFrame(fmd.ident, time, all_traces, tick);
+    if (! fmd.tag.empty()) {
+        sframe->tag_frame(fmd.tag);
     }
+
+    log->debug("call={}, load frame={} ntraces={} tag=\"{}\" file={}",
+               m_count, fmd.ident, all_traces.size(), fmd.tag, m_inname);
+
     return IFrame::pointer{sframe};
 }
 
