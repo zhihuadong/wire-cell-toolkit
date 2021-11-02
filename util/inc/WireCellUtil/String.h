a9787bb9d858c2665429b2ae095576097cbade8f
Brett Viren <brett.viren@gmail.com>
2021-11-02 12:50:57 -0400

    Tell Boost to shut up with the internal deprecation warnings

--- util/inc/WireCellUtil/String.h
+++ util/inc/WireCellUtil/String.h
@@ -1,6 +1,18 @@
 #ifndef WIRECELLUTIL_STRING
 #define WIRECELLUTIL_STRING
 
+// fixme: watchme: Boost started to deprecate some internal header
+// inclusion which is not, as best as I can tell, any of our problem.
+// The message is:
+//
+// ../../../../../opt/boost-1-76-0/include/boost/config/pragma_message.hpp:24:34: note: ‘#pragma message: This header is deprecated. Use <iterator> instead.’
+//
+//  This arises from a deeply nested #include well beyond anything
+//  which is obvious here.
+//
+//  If/when this is cleaned up in Boost, remove this comment and the
+//  next line.
+#define BOOST_ALLOW_DEPRECATED_HEADERS 1
 #include <boost/algorithm/string.hpp>
 #include <boost/format.hpp>
 
